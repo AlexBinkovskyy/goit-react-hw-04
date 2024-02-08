87557fff9393ef7680aa143a2109e666d9ccbef7
AlexBinkovskyy <alex_bin@ukr.net>
2024-02-08 22:25:18 +0200

    header style

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,22 @@
 import { useId } from 'react';
+import style from './SearchBar.module.css'
 
 export const SearchBar = ({ onSubmit }) => {
   const inputId = useId();
   return (
-    <header>
-      <form onSubmit={onSubmit} >
-        <input
-          id={inputId}
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-        <button type="submit">Search</button>
+    <header className={style.header}>
+      <form onSubmit={onSubmit} className={style.form}>
+        <div className={style.wrapper}>
+          <input
+            id={inputId}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            className={style.input}
+          />
+          <button type="submit"className={style.btn}>Search</button>
+        </div>
       </form>
     </header>
   );
